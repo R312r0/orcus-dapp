ce61d4efcca03f037b961c97c693abcafe10ba97
SergeiKanerva <49556048+SergeiKanerva@users.noreply.github.com>
2022-04-13 13:24:59 +0400

    Profit Manager Page, Recollateralize Page

--- src/pages/ProfitManager/index.jsx
+++ src/pages/ProfitManager/index.jsx
@@ -1,9 +1,106 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import { ProfitManagerWrapper } from './styled';
+import { ProfitManagerWrapper, HeadingText, HistoryHeader, HistoryTableRow,HistoryTableHead,HistoryContainer, HorizontalSpaceBetween,THRButton, Text,Line, THRValue, THRLabel,THRContainer, TPPLabel, ProfitManagerHeader, VerticalSpaceBetween, THRContent, HistoryContent  } from './styled';
 
 const ProfitManager = () => {
-  return <ProfitManagerWrapper></ProfitManagerWrapper>;
+
+  const [ historyArray ,  ] = useState([{date: '2/19/2022', fee: '4,468 ORU', collateral: '0 ORU', arbitrager: '1,121 ORU', penalty: '33,057 ORU', totalORU: '38,645 ORU', totalUSD: '$ 4,048', txHash: '0x7025...edc0'},{date: '2/19/2022', fee: '4,468 ORU', collateral: '0 ORU', arbitrager: '1,121 ORU', penalty: '33,057 ORU', totalORU: '38,645 ORU', totalUSD: '$ 4,048', txHash: '0x7025...edc0'},])
+
+  return <ProfitManagerWrapper>
+    <ProfitManagerHeader>
+    <HorizontalSpaceBetween>
+      <div>
+        <VerticalSpaceBetween>
+          <div>
+          <HeadingText>Orcus Profit Manager</HeadingText>
+          </div>
+          <div>
+          <Text>Total Protocol Profit</Text>
+          <TPPLabel>$ 29,679</TPPLabel>
+          </div>
+        </VerticalSpaceBetween>
+      </div>
+      <div>
+        <THRContainer>
+          <THRContent>
+            <THRLabel>Total harvested rewards </THRLabel>
+            <Line color='white'></Line>
+            <THRValue>0.0 ORU</THRValue>
+          </THRContent>
+        </THRContainer>
+        <THRButton>
+        Rewards vesting
+        </THRButton>
+        </div>
+    </HorizontalSpaceBetween>
+    </ProfitManagerHeader>
+    <HistoryContainer>
+      <HistoryContent  paddingTop='32px'>
+        <HistoryHeader>History</HistoryHeader>
+        <HistoryTableHead>
+          <div>
+            Date
+          </div>
+          <div>
+            Fee Collector
+          </div>
+          <div>
+          Collateral Invest
+          </div>
+          <div>
+          Arbitrager
+          </div>
+          <div>
+          Penalty
+          </div>
+          <div>
+          TOTAL in ORU
+          </div>
+          <div>
+          TOTAL in $
+          </div>
+          <div>
+          Tx
+          </div>
+        </HistoryTableHead>
+      </HistoryContent>
+        <Line color='#F2F2F2'></Line>
+        <HistoryContent paddingTop='0px'>
+
+          { historyArray.map(row => {
+            return (<HistoryTableRow key={Math.random()}>
+
+          
+              <div>
+                  {row.date}
+                </div>
+                <div>
+                  {row.fee}
+                </div>
+                <div>
+                {row.collateral}
+                </div>
+                <div>
+                {row.arbitrager}
+                </div>
+                <div>
+                {row.penalty}
+                </div>
+                <div>
+                {row.totalORU}
+                </div>
+                <div>
+                {row.totalUSD}
+                </div>
+                <div>
+                {row.txHash}
+                </div>
+                </HistoryTableRow>);
+          })}
+          
+        </HistoryContent>
+    </HistoryContainer>
+  </ProfitManagerWrapper>;
 };
 
 export default ProfitManager;
--- src/pages/ProfitManager/styled.js
+++ src/pages/ProfitManager/styled.js
@@ -1,15 +1,137 @@
 import styled from 'styled-components';
 
 export const ProfitManagerWrapper = styled.div`
-  flex-direction: row;
+  flex-direction: column;
   display: flex;
+  width: 100%;
 `;
 
 export const HeadingText = styled.span`
   font-family: 'Poppins';
   line-height: 1.875vw;
-  margin-left: 1.25vw;
+
   font-size: 1.25vw;
   font-weight: 500;
   color: #333;
+`
+
+
+export const ProfitManagerHeader = styled.div`
+  width: 100%;  
+  height: 144px;
+  display: flex
+`
+
+export const Text = styled.span`
+  font-family: ${(props) => props.fontFamily ?? 'Poppins'};
+  margin-left: ${(props) => props.ml};
+  margin-top: ${(props) => props.mt};
+  line-height: 1.094vw;
+  font-size: 0.729vw;
+  font-weight: 400;
+  color: #828282;
+`
+export const HorizontalSpaceBetween = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`
+export const VerticalSpaceBetween = styled.div`
+display: flex;
+justify-content: space-between;
+height: 100%;
+flex-direction: column;
+`
+export const TPPLabel = styled.div`
+font-family: ${(props) => props.fontFamily ?? 'Poppins'};
+  font-size: 1.875vw;
+  color: #333333;
+  font-weight: 600;
+`
+
+export const THRContainer = styled.div`
+  height: 86px;
+  width: 297px;
+  background-color: #E4DDEF;
+  text-align: right;
+  border-radius: 20px;
+`
+export const THRContent = styled.div`
+  padding: 13px 25px;
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  flex-direction: column;
+`
+
+export const THRLabel = styled.div`
+  font-size:0.729vw;
+  color: #4F4F4F;
+`
+
+ export const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: ${props => props.color ?? 'white'};
+ `
+ export const THRValue = styled.div`
+ font-size: 0.937vw;
+ color: #333;
+ font-weight: 600;
+ `
+
+ export const THRButton = styled.button`
+  border: none;
+  border-radius: 20px;
+  font-size: 0.875vw;
+  color: white;
+  background-color: #333333;
+  width :100%;
+  height: 46px;
+  margin-top: 12px;
+ `
+ export const HistoryContainer = styled.div`
+  width: 100%;
+  margin-top: 0.9vw;
+  flex-direction: column;
+  height: 320px;
+  border-radius: 1.042vw;
+  overflow-x: hidden;
+  overflow-y: auto;
+  display: flex;
+  background-color: white;
+  border-radius: 20px;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
+export const HistoryContent = styled.div`
+
+padding-top: ${props => props.paddingTop};
+padding-left: 72px;
+padding-right: 72px;
+`
+export const HistoryHeader = styled.div`
+  font-size: 1.25vw;
+  font-family: 'Poppins';
+  font-weight: 500;
+  color: #333333;
+`
+export const HistoryTableHead = styled.div`
+  margin-top: 24px;
+  font-size: 0.875vw;
+  color: #333333;
+  font-weight: 500;
+  display: grid;
+  grid-template-columns: 1fr 2fr 2fr 2fr 1fr 2fr 2fr 1fr;
+  padding-bottom: 8px;
+`
+export const HistoryTableRow = styled.div`
+  margin-top: 8px;
+  font-size: 0.729vw;
+  color: #4F4F4F;
+  font-weight: 500;
+  display: grid;
+  grid-template-columns: 1fr 2fr 2fr 2fr 1fr 2fr 2fr 1fr;
+  padding-bottom: 8px;
+`
